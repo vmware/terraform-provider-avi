8f853e67f12352a37e5dc49430453526a083e35a
Gaurav Rastogi <grastogi@avinetworks.com>
2018-01-05 08:18:00 -0800

    updated example with more datasource examples

--- examples/avi_example.tf
+++ examples/avi_example.tf
@@ -5,18 +5,49 @@ provider "avi" {
   avi_controller= "10.10.25.42"
 }
 
-data "avi_applicationprofile" "system_app_profile" {
+data "avi_applicationprofile" "system_http_profile" {
   name= "System-HTTP"
-  type= "APPLICATION_PROFILE_TYPE_HTTP"
 }
 
+data "avi_applicationprofile" "system_https_profile" {
+  name= "System-Secure-HTTP"
+}
 
-output "system app profile" {
-  value = "${data.avi_applicationprofile.system_app_profile.name}"
+data "avi_tenant" "default_tenant" {
+  name= "admin"
+}
+
+data "avi_cloud" "default_cloud" {
+  name= "Default-Cloud"
+}
+
+data "avi_serviceenginegroup" "se_group" {
+  name = "Default-Group"
+}
+
+data "avi_networkprofile" "system_tcp_profile" {
+  name= "System-TCP-Proxy"
+}
+
+data "avi_analyticsprofile" "system_analytics_profile" {
+  name= "System-Analytics-Profile"
+}
+
+data "avi_sslkeyandcertificate" "system_default_cert" {
+  name= "System-Default-Cert"
+}
+
+data "avi_sslprofile" "system_standard_sslprofile" {
+  name= "System-Standard"
+}
+
+data "avi_vrfcontext" "global_vrf" {
+  name= "global"
 }
 
 resource "avi_networkprofile" "test_networkprofile" {
   name= "networkprofile-42"
+  tenant_ref= "${data.avi_tenant.default_tenant.id}"
   profile{
     type= "PROTOCOL_TYPE_TCP_PROXY"
   }
@@ -24,6 +55,7 @@ resource "avi_networkprofile" "test_networkprofile" {
 
 resource "avi_applicationpersistenceprofile" "test_applicationpersistenceprofile" {
   name = "applicationpersistence-42"
+  tenant_ref= "${data.avi_tenant.default_tenant.id}"
   persistence_type = "PERSISTENCE_TYPE_CLIENT_IP_ADDRESS"
 }
 
@@ -41,9 +73,10 @@ resource "avi_vsvip" "test_vsvip" {
 resource "avi_virtualservice" "test_vs" {
   name= "vs-42"
   pool_ref= "${avi_pool.testpool.id}"
-  application_profile_ref= "${data.avi_applicationprofile.system_app_profile.id}"
-  #application_profile_ref= "${avi_applicationprofile.test_applicationprofile.id}"
-  network_profile_ref = "${avi_networkprofile.test_networkprofile.id}"
+  tenant_ref= "${data.avi_tenant.default_tenant.id}"
+  cloud_ref= "${data.avi_cloud.default_cloud.id}"
+  application_profile_ref= "${data.avi_applicationprofile.system_https_profile.id}"
+  network_profile_ref = "${data.avi_networkprofile.system_tcp_profile.id}"
   vsvip_ref = "${avi_vsvip.test_vsvip.id}"
   vip {
     vip_id= "0"
@@ -55,13 +88,16 @@ resource "avi_virtualservice" "test_vs" {
   services {
     port= 80
     enable_ssl= true
+    port_range_end= 80
   }
+  cloud_type = "CLOUD_VCENTER"
+  se_group_ref= "${data.avi_serviceenginegroup.se_group.id}"
+  analytics_profile_ref= "${data.avi_analyticsprofile.system_analytics_profile.id}"
+  ssl_key_and_certificate_refs= ["${data.avi_sslkeyandcertificate.system_default_cert.id}"]
+  ssl_profile_ref= "${data.avi_sslprofile.system_standard_sslprofile.id}"
+  vrf_context_ref= "${data.avi_vrfcontext.global_vrf.id}"
 }
 
-resource "avi_applicationprofile" "test_applicationprofile" {
-  name= "applicationprofile-42"
-  type= "APPLICATION_PROFILE_TYPE_DNS"
-}
 
 resource "avi_healthmonitor" "test_hm_1" {
   name = "healthmonitor-42"
@@ -72,6 +108,8 @@ resource "avi_healthmonitor" "test_hm_1" {
 resource "avi_pool" "testpool" {
   name= "pool-42",
   health_monitor_refs= ["${avi_healthmonitor.test_hm_1.id}"]
+  tenant_ref= "${data.avi_tenant.default_tenant.id}"
+  cloud_ref= "${data.avi_cloud.default_cloud.id}"
   application_persistence_profile_ref= "${avi_applicationpersistenceprofile.test_applicationpersistenceprofile.id}"
   servers {
     ip= {
