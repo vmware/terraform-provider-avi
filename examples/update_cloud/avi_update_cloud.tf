846a4ccef79f3e4711f458caec041f797341a72b
Shrikant Patil <shrikant.patil@opcito.com>
2018-06-27 02:23:04 +0530

    Added tf script to create configuration on controller and update defa… (#59)
    * Added tf script to create configuration on controller and update default-cloud to Azure cloud
    
    * Updated cloud credential ref

---
+++ examples/update_cloud/avi_update_cloud.tf
@@ -0,0 +1,108 @@
+
+provider "avi" {
+  avi_username = "${var.avi_username}"
+  avi_tenant = "${var.avi_tenant}"
+  avi_password = "${var.avi_password}"
+  avi_controller= "${var.avi_controller}"
+}
+
+resource "avi_serviceenginegroup" "test_se" {
+	name= "Default-Group1",
+	license_type= "LIC_SE_BANDWIDTH",
+	se_bandwidth_type= "SE_BANDWIDTH_200M",
+	algo= "PLACEMENT_ALGO_PACKED"
+}
+
+resource "avi_tenant" "test_tenant" {
+  name= "test-tenant"
+}
+
+resource "avi_tenant" "avi_tenant" {
+  name= "avinetworks"
+}
+
+resource "avi_sslprofile" "system_standard_sslprofile" {
+  name= "System-Standard",
+  accepted_versions= {
+		"type"= "SSL_VERSION_TLS1"
+	},
+  accepted_versions= {
+		"type"= "SSL_VERSION_TLS1_2"
+	},
+  accepted_ciphers= "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES256-SHA384"
+}
+
+data "avi_tenant" "default_tenant"{
+	name= "admin"
+}
+
+resource "avi_role" "testRole" {
+"privileges" {
+"resource" = "PERMISSION_CONTROLLER"
+"type" = "NO_ACCESS"
+}
+"privileges" {
+"resource" = "PERMISSION_VIRTUALSERVICE"
+"type" = "READ_ACCESS"
+}
+"privileges" {
+"resource" = "PERMISSION_POOL"
+"type" = "READ_ACCESS"
+}
+"privileges" {
+"resource" = "PERMISSION_POOLGROUP"
+"type" = "READ_ACCESS"
+}
+"tenant_ref" = "${data.avi_tenant.default_tenant.id}"
+"name" = "testApplication-Admin"
+}
+
+
+resource "avi_alertemailconfig" "testalertemailconfig" {
+	name = "aec-abc"
+	tenant_ref= "${data.avi_tenant.default_tenant.id}"
+	cc_emails= "admin@avicontroller.net"
+	description= "test alert email"
+	to_emails= "admin@avicontroller.net"
+}
+
+resource "avi_alertsyslogconfig" "testalertsyslogconfig" {
+	name = "asyc-test"
+	description= "test alert syslog"
+	tenant_ref= "${data.avi_tenant.default_tenant.id}"
+}
+
+resource "avi_cloudconnectoruser" "custom_cloud_ref" {
+    azure_userpass {
+        password = "${var.azure_password}"
+        username = "${var.azure_username}"
+        tenant_name = "${var.azure_tenant_name}"
+    }
+    name = "test_user"
+}
+
+resource "avi_cloud" "azurcloud" {
+	vtype = "CLOUD_AZURE"
+	license_tier = "ENTERPRISE_18"
+	dhcp_enabled = true
+	prefer_static_routes = false
+	license_type = "LIC_CORES"
+	mtu = 1500
+	apic_mode = false
+	state_based_dns_registration = true
+	name = "Default-Cloud"
+	azure_configuration {	
+		location = "westus"
+		resource_group = "${var.resource_group}"
+		subscription_id = "${var.subscription_id}"
+		use_azure_dns = "true"
+		cloud_credentials_ref = "${avi_cloudconnectoruser.custom_cloud_ref.id}"
+		network_info = [
+		{
+			se_network_id = "default",
+			virtual_network_id = "${var.virtual_network_id}"
+		}
+		]
+	}
+}
+
---
+++ examples/update_cloud/variables.tf
@@ -0,0 +1,43 @@
+
+variable "avi_controller" {
+  description = "Enter Controller IP"
+}
+
+variable "avi_username" {
+  description = "Enter Controller Username"
+}
+
+variable "avi_password" {
+  description = "Enter Controller Password"
+}
+
+variable "avi_tenant" {
+  description = "Enter tenant name"
+  default = "admin"
+}
+
+variable "subscription_id" {
+  description = "Enter Subscription ID for provisioning resources in Azure"
+}
+
+variable "resource_group" {
+  description = "Enter Resource group for provisioning resources in Azure"
+}
+
+variable "virtual_network_id" {
+  description = "Enter virtual network id for provisioning resources in Azure"
+}
+
+variable "azure_username" {
+  description = "Enter azure username"
+}
+
+variable "azure_password" {
+  description = "Enter azure password"
+}
+
+variable "azure_tenant_name" {
+  description = "Enter azure tenant"
+}
+
+
