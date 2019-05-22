9f9a07fdd97772020bfcca68e6679920cb940002
shrikant patil <shrikant.patil@avinetworks.com>
2019-05-22 11:17:54 +0530

    Terr tests (#121)
    * Added python script to execute terraform tests
    
    * Updated test comand
    
    * Updated cmd
    
    * AV-63165 test_terraform_acc.py tcid addition

---
+++ test/test_terraform_acc.py
@@ -0,0 +1,14 @@
+import shlex
+import logging
+import subprocess
+logging.basicConfig(filename='terraform_tests.log', level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+
+
+@pytest.mark.TCID1_48_1607_1_0
+def test_terraform_acc():
+    cmd = "make testacc"
+    LOG.info("executing command %s ", cmd)
+    out = subprocess.check_output(shlex.split(cmd))
+    LOG.info("terraform testacc : out %s ", out)
