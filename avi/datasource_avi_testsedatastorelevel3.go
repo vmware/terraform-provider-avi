2d841f4a2f44c47f7c9c6ff553d5216b21d015d3
Rob Lazzurs <rob@lazzurs.org>
2019-12-10 00:23:02 +0000

    Migrating to the Terraform Plugin SDK.
    Migrating to the Terraform Plugin SDK using the automated migration
    tool.
    
    Build and tests pass.
    
    Adding vendor modules

--- avi/datasource_avi_testsedatastorelevel3.go
+++ avi/datasource_avi_testsedatastorelevel3.go
@@ -5,7 +5,7 @@
  */
 package avi
 
-import "github.com/hashicorp/terraform/helper/schema"
+import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
 func dataSourceAviTestSeDatastoreLevel3() *schema.Resource {
 	return &schema.Resource{
--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.12
 require (
 	github.com/avinetworks/sdk v0.0.0-20191024124359-ed2a110d0b66
 	github.com/hashicorp/go-multierror v1.0.0
-	github.com/hashicorp/terraform v0.12.2
+	github.com/hashicorp/terraform-plugin-sdk v1.1.0
 )
