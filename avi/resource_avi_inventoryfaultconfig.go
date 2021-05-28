06802b719983f4948668ff1c2e09d068f9061e19
Yograj Shisode <yshisode@vmware.com>
2021-05-27 20:44:39 -0700

    Updated assets for terraform
    Signed-off-by: Yograj Shisode <yshisode@vmware.com>

---
+++ avi/resource_avi_inventoryfaultconfig.go
@@ -0,0 +1,115 @@
+// Copyright 2019 VMware, Inc.
+// SPDX-License-Identifier: Mozilla Public License 2.0
+
+package avi
+
+import (
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/vmware/alb-sdk/go/clients"
+)
+
+func ResourceInventoryFaultConfigSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"controller_faults": {
+			Type:     schema.TypeSet,
+			Optional: true,
+			Computed: true,
+			Elem:     ResourceControllerFaultsSchema(),
+		},
+		"name": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"serviceengine_faults": {
+			Type:     schema.TypeSet,
+			Optional: true,
+			Computed: true,
+			Elem:     ResourceServiceengineFaultsSchema(),
+		},
+		"tenant_ref": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"uuid": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Computed: true,
+		},
+		"virtualservice_faults": {
+			Type:     schema.TypeSet,
+			Optional: true,
+			Computed: true,
+			Elem:     ResourceVirtualserviceFaultsSchema(),
+		},
+	}
+}
+
+func resourceAviInventoryFaultConfig() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAviInventoryFaultConfigCreate,
+		Read:   ResourceAviInventoryFaultConfigRead,
+		Update: resourceAviInventoryFaultConfigUpdate,
+		Delete: resourceAviInventoryFaultConfigDelete,
+		Schema: ResourceInventoryFaultConfigSchema(),
+		Importer: &schema.ResourceImporter{
+			State: ResourceInventoryFaultConfigImporter,
+		},
+	}
+}
+
+func ResourceInventoryFaultConfigImporter(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+	s := ResourceInventoryFaultConfigSchema()
+	return ResourceImporter(d, m, "inventoryfaultconfig", s)
+}
+
+func ResourceAviInventoryFaultConfigRead(d *schema.ResourceData, meta interface{}) error {
+	s := ResourceInventoryFaultConfigSchema()
+	err := APIRead(d, meta, "inventoryfaultconfig", s)
+	if err != nil {
+		log.Printf("[ERROR] in reading object %v\n", err)
+	}
+	return err
+}
+
+func resourceAviInventoryFaultConfigCreate(d *schema.ResourceData, meta interface{}) error {
+	s := ResourceInventoryFaultConfigSchema()
+	err := APICreateOrUpdate(d, meta, "inventoryfaultconfig", s)
+	if err == nil {
+		err = ResourceAviInventoryFaultConfigRead(d, meta)
+	}
+	return err
+}
+
+func resourceAviInventoryFaultConfigUpdate(d *schema.ResourceData, meta interface{}) error {
+	s := ResourceInventoryFaultConfigSchema()
+	var err error
+	err = APICreateOrUpdate(d, meta, "inventoryfaultconfig", s)
+	if err == nil {
+		err = ResourceAviInventoryFaultConfigRead(d, meta)
+	}
+	return err
+}
+
+func resourceAviInventoryFaultConfigDelete(d *schema.ResourceData, meta interface{}) error {
+	objType := "inventoryfaultconfig"
+	client := meta.(*clients.AviClient)
+	if APIDeleteSystemDefaultCheck(d) {
+		return nil
+	}
+	uuid := d.Get("uuid").(string)
+	if uuid != "" {
+		path := "api/" + objType + "/" + uuid
+		err := client.AviSession.Delete(path)
+		if err != nil && !(strings.Contains(err.Error(), "404") || strings.Contains(err.Error(), "204") || strings.Contains(err.Error(), "403")) {
+			log.Println("[INFO] resourceAviInventoryFaultConfigDelete not found")
+			return err
+		}
+		d.SetId("")
+	}
+	return nil
+}
--- avi/resource_avi_rest_dependants.go
+++ avi/resource_avi_rest_dependants.go
@@ -6254,6 +6254,43 @@ func ResourceControllerDiscontinuousTimeChangeEventDetailsSchema() *schema.Resou
 	}
 }
 
+func ResourceControllerFaultsSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"backup_scheduler_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"cluster_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"deprecated_api_version_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"license_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"migration_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"sslprofile_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
 func ResourceControllerLicenseSchema() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
@@ -27034,6 +27071,18 @@ func ResourceServicePoolSelectorSchema() *schema.Resource {
 	}
 }
 
+func ResourceServiceengineFaultsSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"debug_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
 func ResourceSidebandProfileSchema() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
@@ -31002,6 +31051,43 @@ func ResourceVirtualServiceRuntimeSchema() *schema.Resource {
 				Optional: true,
 				Computed: true,
 				Elem:     ResourceVirtualServiceSchema(),
+			},
+		},
+	}
+}
+
+func ResourceVirtualserviceFaultsSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"debug_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"pool_server_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"scaleout_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"shared_vip_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"ssl_cert_expiry_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"ssl_cert_status_faults": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
 			},
 		},
 	}
