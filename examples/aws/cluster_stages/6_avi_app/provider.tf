b0b6b9d77bd55f42156a70cfeed5f40b0e07eb9f
Remo Mattei <remo@mattei.org>
2021-11-18 14:33:38 -0800

    adding terraform avi provider

--- examples/aws/cluster_stages/6_avi_app/provider.tf
+++ examples/aws/cluster_stages/6_avi_app/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    avi = {
+      source  = "vmware/avi"
+      version = "21.1.1"
+    }
+  }
+}
 provider "aws" {
   #access_key = var.aws_access_key
   #secret_key = var.aws_secret_key
