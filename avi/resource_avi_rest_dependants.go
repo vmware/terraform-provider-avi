1b2369031f2ec1da750c694867f99ff0b6986df4
Yograj Shisode <yshisode@vmware.com>
2022-08-17 01:46:05 -0700

    Updated assets for terraform
    Signed-off-by: Yograj Shisode <yshisode@vmware.com>

--- avi/resource_avi_rest_dependants.go
+++ avi/resource_avi_rest_dependants.go
@@ -23101,6 +23101,18 @@ func ResourceOpenStackConfigurationSchema() *schema.Resource {
 				Optional: true,
 				Elem:     ResourcePropertySchema(),
 			},
+			"enable_os_object_caching": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "false",
+				ValidateFunc: validateBool,
+			},
+			"enable_tagging": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "false",
+				ValidateFunc: validateBool,
+			},
 			"external_networks": {
 				Type:         schema.TypeString,
 				Optional:     true,
@@ -23236,6 +23248,12 @@ func ResourceOpenStackConfigurationSchema() *schema.Resource {
 			"username": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"vip_port_in_admin_tenant": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "false",
+				ValidateFunc: validateBool,
 			},
 		},
 	}
