501a5e1d58d43f1f141ab9dd6548a1f49db556da
Rohan <rsuryavanshi@vmware.com>
2023-09-04 14:47:42 +0000

    Fixed goimport issue

--- avi/resource_avi_testsedatastorelevel3.go
+++ avi/resource_avi_testsedatastorelevel3.go
@@ -4,8 +4,9 @@
 package avi
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func ResourceTestSeDatastoreLevel3Schema() map[string]*schema.Schema {
