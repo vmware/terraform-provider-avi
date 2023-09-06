7b3adaa30eaeeb062659e86450ed5e7391928b00
Sagar <ssalvi@vmware.com>
2023-09-05 20:43:13 -0700

    Updated assets for terraform
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- avi/resource_avi_alertconfig.go
+++ avi/resource_avi_alertconfig.go
@@ -4,9 +4,8 @@
 package avi
 
 import (
-	"log"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"log"
 )
 
 func ResourceAlertConfigSchema() map[string]*schema.Schema {
--- avi/resource_avi_alertscriptconfig.go
+++ avi/resource_avi_alertscriptconfig.go
@@ -4,9 +4,8 @@
 package avi
 
 import (
-	"log"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"log"
 )
 
 func ResourceAlertScriptConfigSchema() map[string]*schema.Schema {
