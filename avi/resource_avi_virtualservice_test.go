d059a7398a745db24755cb8a93be5a08612a4ffb
avijenkins <svc.avi-jenkins@broadcom.com>
2025-09-15 09:03:12 +0000

    Updated assets for terraform
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- avi/resource_avi_virtualservice_test.go
+++ avi/resource_avi_virtualservice_test.go
@@ -85,12 +85,6 @@ func testAccCheckAVIVirtualServiceValuesUpdated(vs *models.VirtualService, key s
 		if key == "name" && *vs.Name != val {
 			return fmt.Errorf("bad name, expected \"%v\", got: %#v", val, *vs.Name)
 		}
-		if key == "vip.0.enabled" && *vs.Vip[0].Enabled != val {
-			return fmt.Errorf("bad vip enabled, expected \"%v\", got: %#v", val, *vs.Vip[0].Enabled)
-		}
-		if key == "vip.0.vip_id" && *vs.Vip[0].VipID != val {
-			return fmt.Errorf("bad vip_id, expected \"%v\", got: %#v", val, *vs.Vip[0].VipID)
-		}
 		if key == "min_pools_up" && int(*vs.MinPoolsUp) != val {
 			return fmt.Errorf("bad min_pools_up, expected \"%v\", got: %v", val, *vs.MinPoolsUp)
 		}
