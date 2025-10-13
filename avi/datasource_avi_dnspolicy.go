c3f9c0e6b5420a7fd7ea9fee711bb29089879431
avijenkins <svc.avi-jenkins@broadcom.com>
2025-10-13 09:02:19 +0000

    Updated assets for terraform
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- avi/datasource_avi_dnspolicy.go
+++ avi/datasource_avi_dnspolicy.go
@@ -5,7 +5,7 @@ package avi
 
 import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-// nolint
+//nolint
 func dataSourceAviDnsPolicy() *schema.Resource {
 	return &schema.Resource{
 		Read: ResourceAviDnsPolicyRead,
