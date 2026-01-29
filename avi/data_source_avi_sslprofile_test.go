5ede2ec500ed733089c8008ab1b1209287704717
avijenkins <svc.avi-jenkins@broadcom.com>
2026-01-29 09:07:36 +0000

    Updated assets for terraform
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>
    Signed-off-by: Rohan <rohan.suryavanshi@broadcom.com>

--- avi/data_source_avi_sslprofile_test.go
+++ avi/data_source_avi_sslprofile_test.go
@@ -42,7 +42,7 @@ resource "avi_sslprofile" "testSSLProfile" {
 	name = "test-System-Standard-abc"
 	tenant_ref = data.avi_tenant.default_tenant.id
 	ssl_session_timeout = "86400"
-	accepted_ciphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384"
+	accepted_ciphers = "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:AES128-SHA"
 	prefer_client_cipher_ordering = false
 	accepted_versions {
 	type = "SSL_VERSION_TLS1"
@@ -54,12 +54,12 @@ accepted_versions {
 	type = "SSL_VERSION_TLS1_2"
 }
 	enable_ssl_session_reuse = true
-	cipher_enums = ["TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256","TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384","TLS_AES_256_GCM_SHA384","TLS_CHACHA20_POLY1305_SHA256","TLS_AES_128_GCM_SHA256"]
+	cipher_enums = ["TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256","TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384","TLS_RSA_WITH_AES_128_CBC_SHA","TLS_AES_256_GCM_SHA384","TLS_CHACHA20_POLY1305_SHA256","TLS_AES_128_GCM_SHA256"]
 	send_close_notify = true
 	ssl_rating {
 		performance_rating = "SSL_SCORE_EXCELLENT"
 		security_score = "100.0"
-		compatibility_rating = "SSL_SCORE_GOOD"
+		compatibility_rating = "SSL_SCORE_EXCELLENT"
 	}
 	dhparam = <<EOF
 -----BEGIN DH PARAMETERS-----
